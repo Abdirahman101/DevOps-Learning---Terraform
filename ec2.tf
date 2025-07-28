c93368c1a7ba21bb26b212186ddcb848f0f8c139
Abdirahman101 <abdirahman.ahmed091@gmail.com>
2025-07-28 15:08:33 +0100

    add updated files

--- ec2.tf
+++ ec2.tf
@@ -2,3 +2,12 @@ resource "aws_instance" "this" {
   ami                     = "ami-0175d4f2509d1d9e8"
   instance_type           = "t3.micro"
 }
+
+resource "aws_instance" "import" {
+  ami                     = "ami-0175d4f2509d1d9e8"
+  instance_type           = "t3.micro"
+  tags = {
+    Name = "terraform_import"
+  }
+  user_data_replace_on_change = false
+}
